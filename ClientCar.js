6aa5817f673180adf8494db4787bfb42e61c0e31
harisris <haris.coliche@etu.univ-smb.fr>
2022-02-16 11:30:38 +0100

    heroku config

--- ClientCar.js
+++ ClientCar.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 3000;
 
 const url = 'https://voitur.herokuapp.com/?wsdl';
 const soap = require('soap');
